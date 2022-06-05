667b67a566908732776f699843d132d1c0894686
James Stone <jamesmstone@hotmail.com>
2022-06-05 18:16:52 +1000

    fix tailscale

--- home/services/tailscale/default.nix
+++ home/services/tailscale/default.nix
@@ -1,6 +1,9 @@
-{
-  services.tailscale = {
-    enable = true;
-  };
-}
+{ config, pkgs, ... }:
 
+{
+  # make the tailscale command usable to users
+  environment.systemPackages = [ pkgs.tailscale ];
+
+  # enable the tailscale service
+  services.tailscale.enable = true;
+}
